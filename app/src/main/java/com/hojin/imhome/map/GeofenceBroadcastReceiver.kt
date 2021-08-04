81fb1b6c986dde6e55f9a777ab0fa0d72873d93a
cri3958 <leehojin0207@naver.com>
2021-08-04 20:46:29 +0900

    지도 10 일단저장

---
+++ app/src/main/java/com/hojin/imhome/map/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,49 @@
+package com.hojin.imhome.map
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.location.GeofencingEvent
+
+class GeofenceBroadcastReceiver : BroadcastReceiver() {
+    val TAG = GeofenceBroadcastReceiver::class.java.simpleName
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+        if (geofencingEvent.hasError()) {
+            val errorMessage = GeofenceStatusCodes
+                .getStatusCodeString(geofencingEvent.errorCode)
+            Log.e(TAG, errorMessage)
+            return
+        }
+
+        // Get the transition type.
+        val geofenceTransition = geofencingEvent.geofenceTransition
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+        geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            val triggeringGeofences = geofencingEvent.triggeringGeofences
+
+            // Get the transition details as a String.
+            val geofenceTransitionDetails = getGeofenceTransitionDetails(
+                this,
+                geofenceTransition,
+                triggeringGeofences
+            )
+
+            // Send notification and log the transition details.
+            sendNotification(geofenceTransitionDetails)
+            Log.i(TAG, geofenceTransitionDetails)
+        } else {
+            // Log the error.
+            Log.e(TAG, "$TAG 에서 에러발생")
+        }
+    }
+}
