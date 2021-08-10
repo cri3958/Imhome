76f5de2f6831ba26d5c4e6931c0d4ff3d422bf40
cri3958 <leehojin0207@naver.com>
2021-08-10 20:55:57 +0900

    지도 12 속보) 안드로이드 정책에 의해 와이파이와 데이터를 어플에서 킬수가 없게됨... 내 계획은?

--- app/src/main/java/com/hojin/imhome/MainActivity.kt
+++ app/src/main/java/com/hojin/imhome/MainActivity.kt
@@ -3,14 +3,18 @@ package com.hojin.imhome
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.hojin.imhome.map.ManifestActivity
 import com.hojin.imhome.map.MapActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +28,12 @@ class MainActivity : AppCompatActivity() {
         if (!notificationManager.isNotificationPolicyAccessGranted) {
             Toast.makeText(this, "권한을 허용해주세요", Toast.LENGTH_LONG).show();
             startActivity(Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS))
+        }
+
+        textView.setOnClickListener {
+            val wifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
+            wifiManager.isWifiEnabled = true
+            Log.d(TAG, "onCreate: turn on wifi")
         }
     }
 }
